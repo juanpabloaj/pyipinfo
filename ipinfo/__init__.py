90b3d2fff9c431c9e987ab52aee6ea214f3612b3
JuanPablo <jpabloaj@gmail.com>
2014-12-28 19:53:33 -0300

    json output to table output

--- ipinfo/__init__.py
+++ ipinfo/__init__.py
@@ -22,8 +22,20 @@ def main():
         exit()
 
     ip_set = set(ip_list)
+
+    ips_info = []
     for ip in ip_set:
-        print request_to_ipinfo(ip)
+        ips_info.append(request_to_ipinfo(ip))
+
+    all_keys = [
+        'ip', 'city', 'region', 'country',
+        'hostname', 'org', 'postal', 'loc'
+    ]
+
+    for info in ips_info:
+        for k in all_keys:
+            print '{}\t'.format(info[k]),
+        print
 
 
 if __name__ == '__main__':
